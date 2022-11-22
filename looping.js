8c239226b4ec7585a5b88d5e1eda0bedae09a7a2
Nasyah pratiwi <118705848+Nsyhprtwi@users.noreply.github.com>
2022-11-22 14:45:30 +0700

    tugas looping for dan while

---
+++ looping.js
@@ -0,0 +1,17 @@
+//menampilkan bilangan genap pake (for)
+for(var i=1 ; i<=10;i++){
+   if(i%2==0){
+       console.log(i);
+   }
+}
+
+
+
+// menampilkan bilangan genap pake (while)
+let i = 0;
+while (i<=10){
+    i++; 
+    if(i%2==0) {
+        console.log(i);
+    }
+}
